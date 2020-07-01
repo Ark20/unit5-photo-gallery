0a8a88d642f6118f034c0de9adb36c1d595b42b8
Audrey Bazyk <audreybazyk@gmail.com>
2020-07-01 09:06:05 -0500

    Added titles and captions
    Added alt titles and captions to images, tested in browser, added all css and js files in html.

--- js/lightgallery.js
+++ js/lightgallery.js
@@ -1,3 +1,22 @@
+/*! lightgallery - v1.7.1 - 2020-06-13
+* http://sachinchoolur.github.io/lightGallery/
+* Copyright (c) 2020 Sachin N; Licensed GPLv3 */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(['jquery'], function (a0) {
+      return (factory(a0));
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require('jquery'));
+  } else {
+    factory(root["jQuery"]);
+  }
+}(this, function ($) {
+
 (function() {
     'use strict';
 
@@ -1354,3 +1373,6 @@
     $.fn.lightGallery.modules = {};
 
 })();
+
+
+}));
